25f52c46d368cd501a5eeb38792234ef9699426e
dafiger <dafiger2000@qq.com>
2019-06-09 11:40:35 +0800

    update to MediatorModule tag:0.1.0

---
+++ MediatorModule/Classes/Header/MediatorModuleHeader.h
@@ -0,0 +1,13 @@
+//
+//  MediatorModuleHeader.h
+//  MediatorModule
+//
+//  Created by dafiger on 2014.
+//  Copyright © 2014 dafiger. All rights reserved.
+//
+
+#ifndef MediatorModuleHeader_h
+#define MediatorModuleHeader_h
+
+
+#endif /* MediatorModuleHeader_h */
